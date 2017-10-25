568abb72bf18524c69ae41d9a7d473c6afed0188
Tanakun <tanakun.wangpong@gmail.com>
2017-10-25 16:19:21 +0700

    Chat page

--- modules/chats/controller/chat.js
+++ modules/chats/controller/chat.js
@@ -1,7 +1,22 @@
 angular
 .module('myApp')
-.controller('ChatController', ['$scope', 
-function ($scope) {
+.controller('ChatController', ['$scope', '$uibModal',
+function ($scope, $uibModal) {
 	console.log('ChatController');
+	$scope.offerPrice = function() {
+		var modalInstance = $uibModal.open({
+			animation: $scope.animationsEnabled,
+			templateUrl: 'offerPriceModal.html',
+			controller: 'OfferPriceModalController as ctrl'
+			});
+	};
 }]);
 
+angular
+.module('myApp')
+.controller('OfferPriceModalController', function ($scope, $uibModal, $uibModalInstance) {
+	console.log('OfferPriceModalController');
+	$scope.closeMD = function() {
+		$uibModalInstance.close(false);
+	};
+});
