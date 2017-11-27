9419a7a2c7741388f96c53771393f5c50ea4ca4a
root <root@ip-172-31-31-132.ap-southeast-1.compute.internal>
2017-11-27 10:11:39 +0700

    update cert and ssl

--- server.js
+++ server.js
@@ -1,16 +1,54 @@
 var express = require('express')
 var app = express();
 var http = require('http');
+var https = require('https');
 var serveStatic = require('serve-static');
+var join = require('path').join;
+var fs = require('fs');
 
+const processEnv = (process.env.NODE_ENV) ? process.env.NODE_ENV : 'development';
 
+const env = {
+    development: {port: 80, protocol: 'http'},
+    staging: {port: 80, protocol: 'http'},
+    production: {port: 443, protocol: 'https'},
+}
 
+function ensureSecure(req, res, next) {
+    if (req.secure) {
+        // OK, continue
+        return next();
+    }
+    ;
+    // console.log(req.hostname)
+    res.redirect('https://' + req.hostname + req.url); // express 4.x
+}
+
+if (env[processEnv].protocol == 'https') {
+    app.all('*', ensureSecure);
+}
 app.use(express.static('./'));
-app.use(serveStatic('public', {'index': ['index.html', 'index.htm']}));
+// app.use(serveStatic('public', {'index': ['index.html', 'index.htm']}));
 
-var port = (process.env.WEB_PORT) ? process.env.WEB_PORT : 80;
+// var port = (process.env.WEB_PORT) ? process.env.WEB_PORT : 80;
+var port = env[processEnv].port;
+
+if (env[processEnv].protocol == 'https') {
+    let options = {
+        key: fs.readFileSync(join(__dirname, './cert/senseino.co.key')),
+        cert: fs.readFileSync(join(__dirname, './cert/senseino.co.crt')),
+        ca: fs.readFileSync(join(__dirname, './cert/senseino.co.ca'))
+    };
+
+    // console.log('Server start https');
+    var server = https.createServer(options, app);
+    server.listen(port, function () {
+        console.log("website start on port: " + port);
+    });
+}
 
 var httpsServer = http.createServer(app);
-httpsServer.listen(port, function () {
-    console.log("Application start on port: " + port);
+httpsServer.listen(80, function () {
+    console.log("website start on port: " + 80);
 });
+
